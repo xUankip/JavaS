dfb3d98da9d6905afca08606ceed14135671eb3f
xU <142771851+xUankip@users.noreply.github.com>
2023-10-16 10:49:43 +0700

    js

---
+++ JS/c02/numeric-variable.js
@@ -0,0 +1,8 @@
+var price;
+var quantity;
+var total;
+price = 5;
+quantity= 14;
+total=price*quantity;
+var el =document.getElementById('cost');
+el.textContent= total +'$';
---
+++ JS/c02/update-variable.js
@@ -0,0 +1,13 @@
+var inStock;
+var shipping;
+inStock= true;
+shipping=false;
+
+inStock = false;
+shipping= true;
+
+var elStock= document.getElementById('stock');
+elStock.className= inStock;
+
+var elShip= document.getElementById('shipping');
+elShip.className=shipping;
