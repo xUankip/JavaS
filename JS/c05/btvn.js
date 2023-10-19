9b852a3b69741af5cdc23b53700cd5b805a2b28b
xU <142771851+xUankip@users.noreply.github.com>
2023-10-20 01:46:31 +0700

    c05

---
+++ JS/c05/btvn.js
@@ -0,0 +1,96 @@
+//Tạo function 
+    function caculator1(){
+        var giaTien1 = 30; //khai báo giá trị
+        var soLuong1 = document.getElementById('soluong1').value;//lấy số liệu từ class
+        var total1 = giaTien1*soLuong1; //khai báo giá trị tổng
+        document.getElementById('total1').innerHTML = total1; // in ra giá trị tổng
+        if(total1>100){
+            total1*=0.95;
+            document.getElementById('total1').classList.add('hong');
+            document.getElementById('total1').classList.remove('vang')
+        }
+        else if(total1<100){
+            total1*=1
+            document.getElementById('total1').classList.add('vang')
+            document.getElementById('total1').classList.remove('hong')
+        }
+    }setInterval(caculator1,100);
+    caculator1();
+    function caculator2(){
+        var giaTien2 = 95;
+        var soLuong2 = document.getElementById('soluong2').value;
+        var total2 = giaTien2*soLuong2;
+        if(total2>300){
+            total2*=0.95;
+            document.getElementById('total2').classList.add('xanh');
+            document.getElementById('total2').classList.remove('vang')
+        }
+        else{
+            total2*=1
+            document.getElementById('total2').classList.add('vang')
+            document.getElementById('total2').classList.remove('xanh')
+        }
+        document.getElementById('total2').textContent=total2;
+    }
+    setInterval(caculator2,100);
+    caculator2();
+    function caculator3(){
+        var giatien3 = 10;
+        var soLuong3 = document.getElementById('soluong3').value;
+        var total3 = giatien3*soLuong3;
+        if(total3>100){
+            total3*=0.95;
+            document.getElementById('total3').classList.add('do');
+            document.getElementById('total3').classList.remove('vang')
+        }
+        else if(total3<100){
+            total3*=1
+            document.getElementById('total3').classList.add('vang')
+            document.getElementById('total3').classList.remove('do')
+        }
+        document.getElementById('total3').innerHTML = total3;
+    }setInterval(caculator3,100);
+    caculator3();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // return caculator1+caculator2+caculator3;
+// function total(){
+// var myca = caculator1();
+// document.getElementById('total1').innerHTML=myca;
+// var myca2 = caculator2();
+// document.getElementById('total2').innerHTML=myca2;
+// var myca3= caculator3();
+// document.getElementById('total3').innerHTML=myca3;
+// var total = myca + myca2 + myca3;
+// document.getElementById('total').innerHTML.innerHTML=total;
+// }
+// function changeColor(){
+//     const texEl=document.getElementById('total1');
+//     const currentColor=texEl.style.color;
+//     if (texEl > 100 && currentColor ==='black'){
+//         texEl.style.color='xanh';
+//     } else{
+//         texEl.style.color='black';
+//     }
+// }
+// setInterval(changeColor,10000)
